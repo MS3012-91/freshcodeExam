8c46aeb57e29ce7c03b988a442dcd1280a7e5733
MariiaSheinyk <mariasheynik@gmail.com>
2024-02-04 20:14:32 +0100

    fix files structure on server - add app

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,8 @@
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const router = require('./router');
 const controller = require('./socketInit');
-const handlerError = require('./handlerError/handler');
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/public', express.static('public'));
-app.use(router);
-app.use(handlerError);
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
