2985e8165ae0c35bd4935b49a5f7e7d5124781c2
MariiaSheinyk <mariasheynik@gmail.com>
2024-02-12 21:46:43 +0100

    fix foundMessage function

--- client/src/components/ContestBrief/ContestBrief.jsx
+++ client/src/components/ContestBrief/ContestBrief.jsx
@@ -5,39 +5,54 @@ import { goToExpandedDialog } from '../../store/slices/chatSlice';
 import Brief from '../Brief/Brief';
 
 const findConversationInfo = (interlocutorId, userStore, chatStore) => {
-  const {messagesPreview} = chatStore;
+  const { messagesPreview } = chatStore;
   const { id } = userStore.data;
   const participants = [id, interlocutorId];
   participants.sort(
     (participant1, participant2) => participant1 - participant2
   );
-  for (let i = 0; i < messagesPreview.length; i++) {
-    if (isEqual(participants, messagesPreview[i].participants)) {
-      return {
-        participants: messagesPreview[i].participants,
-        _id: messagesPreview[i]._id,
-        blackList: messagesPreview[i].blackList,
-        favoriteList: messagesPreview[i].favoriteList,
-      };
-    }
+
+  const foundMessage = messagesPreview.find(message =>
+    isEqual(participants, message.participants)
+  );
+  if (foundMessage) {
+    return {
+      participants: foundMessage.participants,
+      _id: foundMessage._id,
+      blackList: foundMessage.blackList,
+      favoriteList: foundMessage.favoriteList,
+    };
   }
   return null;
 };
 
-const goChat = (contestData, userStore, chatStore, goToExpandedDialog ) => {
-  const {User} = contestData;
+const goChat = (contestData, userStore, chatStore, goToExpandedDialog) => {
+  const { User } = contestData;
   goToExpandedDialog({
     interlocutor: User,
-    conversationData: findConversationInfo(User.id, userStore, chatStore, goToExpandedDialog),
+    conversationData: findConversationInfo(
+      User.id,
+      userStore,
+      chatStore,
+      goToExpandedDialog
+    ),
   });
 };
 
-const ContestBrief = ({ contestData, role, userStore, chatStore, goToExpandedDialog }) => {
+const ContestBrief = ({
+  contestData,
+  role,
+  userStore,
+  chatStore,
+  goToExpandedDialog,
+}) => {
   return (
     <Brief
       contestData={contestData}
       role={role}
-      goChat={() => goChat(contestData, userStore, chatStore, goToExpandedDialog)}
+      goChat={() =>
+        goChat(contestData, userStore, chatStore, goToExpandedDialog)
+      }
     />
   );
 };
