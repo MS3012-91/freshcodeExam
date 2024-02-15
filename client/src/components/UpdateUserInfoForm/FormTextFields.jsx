9e314c628ae29587d98b869561bc6355fcf31c2b
MariiaSheinyk <mariasheynik@gmail.com>
2024-02-15 23:18:27 +0100

    fix users form

--- client/src/components/UpdateUserInfoForm/FormTextFields.jsx
+++ client/src/components/UpdateUserInfoForm/FormTextFields.jsx
@@ -5,9 +5,6 @@ import styles from './UpdateUserInfoForm.module.sass';
 export default function FormTextFields ({fieldName, fieldLabel}) {
   return (
     <div className={styles.container}>
-      <label htmlFor={fieldName} className={styles.label}>
-        {fieldLabel}
-      </label>
       <FormInput
         name={fieldName}
         type='text'
@@ -17,6 +14,7 @@ export default function FormTextFields ({fieldName, fieldLabel}) {
           input: styles.input,
           warning: styles.error,
           notValid: styles.notValid,
+          inputLabel: styles.label
         }}
       />
     </div>
