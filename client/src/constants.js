fb770f97051c8cd0ca69fb3f1190a698783c9de7
MariiaSheinyk <mariasheynik@gmail.com>
2024-02-20 17:02:12 +0100

    fix constants export

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = 5000;
-export default {
+const CONSTANTS = {
   CUSTOMER: 'customer',
   CREATOR: 'creator',
   CONTEST_STATUS_ACTIVE: 'active',
@@ -85,3 +85,5 @@ export default {
     },
   ],
 };
+
+export default CONSTANTS;
